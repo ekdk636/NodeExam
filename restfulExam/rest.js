2b3312d2f988864e46cc5bdbaa098272e4403702
ekdk636 <ekdk636@gmail.com>
2017-09-30 13:08:52 +0900

    rest.js 2

--- restfulExam/rest.js
+++ restfulExam/rest.js
@@ -4,15 +4,59 @@ var bodyParser = require('body-parser');
 var app = express();
 app.use(bodyParser.urlencoded({extended:false}));
 
-var users = [];
+//var users = [];
+
+var mysql = require('mysql');
+var mongodb = require('mongodb');
+
+var connection = mysql.createConnection(
+{
+  	host     : 'localhost',
+  	user     : 'root',
+  	password : 'test1234',
+  	database : 'restful'
+}); 
+
+connection.connect();
 
 app.get('/user', function(req, res)
 {
-	res.send(JSON.stringify(users));
+	//res.send(JSON.stringify(users));
+
+	connection.query('select * from user', function(err, results, fields)
+	{
+		if(err)
+		{
+			res.send(JSON.stringify(err));
+		}
+		else
+		{
+			res.send(JSON.stringify(results));
+		}
+	});
 });
 
 app.get('/user/:id', function(req, res)
 {
+	connection.query('select * from user where id=?', [req.params.id], function(err, results, fields)
+	{
+		if(err)
+		{
+			res.send(JSON.stringify(err));
+		}
+		else
+		{
+			if(results.length > 0)
+			{
+				res.send(JSON.stringify(results[0]));
+			}
+			else
+			{
+				res.send(JSON.stringify({}));
+			}
+		}
+	});
+	/*
 	var select_idx = -1;
 
 	for(var i=0; i<users.length; i++)
@@ -34,10 +78,25 @@ app.get('/user/:id', function(req, res)
 	{
 		res.send(JSON.stringify(users[select_idx]));
 	}
+	*/
 });
 
 app.post('/user', function(req, res)
 {
+	connection.query('insert into user(name, age) values (?, ?)', [req.body.name, req.body.age]
+	, function(err, result)
+	{
+		if(err)
+		{
+			res.send(JSON.stringify(err));
+		}
+		else
+		{
+			res.send(JSON.stringify(result));
+		}
+	});
+
+	/*
 	var name = req.body.name;
 	var age = Number(req.body.age);
 	var obj = {id:users.length+1, name:name, age:age};
@@ -48,10 +107,25 @@ app.post('/user', function(req, res)
 	users.push(obj);
 
 	res.send(JSON.stringify({result:true, api:'add user info'}));
+	*/
 });
 
 app.put('/user/:id', function(req, res)
 {
+	connection.query('update user set name=?, age=? where id=?', [req.body.name, req.body.age, req.params.id]
+	, function(err, result)
+	{
+		if(err)
+		{
+			res.send(JSON.stringify(err));
+		}
+		else
+		{
+			res.send(JSON.stringify(result));
+		}
+	});
+
+	/*
 	var select_idx = -1;
 
 	for(var i=0; i<users.length; i++)
@@ -77,11 +151,25 @@ app.put('/user/:id', function(req, res)
 		users[select_idx] = obj;
 		res.send(JSON.stringify({result:true}));
 	}
+	*/
 	//res.send(JSON.stringify({api:'modify user info'}));
 });
 
 app.delete('/user/:id', function(req, res)
 {
+	connection.query('delete from user where id=?', [req.params.id], function(err, result)
+	{
+		if(err)
+		{
+			res.send(JSON.stringify(err));
+		}
+		else
+		{
+			res.send(JSON.stringify(result));
+		}
+	});
+
+	/*
 	var select_idx = -1;
 
 	for(var i=0; i<users.length; i++)
@@ -104,6 +192,7 @@ app.delete('/user/:id', function(req, res)
 		users.splice(select_idx, 1);
 		res.send(JSON.stringify({result:true}));
 	}
+	*/
 
 	//res.send(JSON.stringify({api:'delete user info', id:req.params.id}));
 });
